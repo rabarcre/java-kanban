7555510e0556780faee42c81a2ad7c81fca97240
rabarcre <yylou@yandex.ru>
2024-09-11 05:35:02 +0300

    Style change

--- src/main/exception/TaskExistsException.java
+++ src/main/exception/TaskExistsException.java
@@ -1,6 +1,6 @@
 package main.exception;
 
-public class TaskExistsException extends RuntimeException{
+public class TaskExistsException extends RuntimeException {
     public TaskExistsException(String message) {
         super(message);
     }
